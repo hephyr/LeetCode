aae4af208d623ed64aba5b75dc592d170f51dca1
Zephyr <ixuhefeng@gmail.com>
2016-10-01 16:00:58 +0800

    rebuild remove element

--- removeElement.cc
+++ removeElement.cc
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        // int n = nums.size();
+        // for (int i = 0; i < n; ++i) {
+        //     if (nums[i] == val) {
+        //         --n;
+        //         for (int j = i; j < n; ++j) {
+        //             nums[j] = nums[j+1];    
+        //         }
+        //         --i;
+        //     }
+        // }
         int n = nums.size();
+        int pos = 0;
         for (int i = 0; i < n; ++i) {
-            if (nums[i] == val) {
-                --n;
-                for (int j = i; j < n; ++j) {
-                    nums[j] = nums[j+1];    
-                }
-                --i;
+            if (nums[i] != val) {
+                nums[pos++] = nums[i];
             }
         }
-        return n;
+        return pos;
     }
 };
