d2ec89e2ff16828c2540ac797d474495edf7b848
Zephyr <ixuhefeng@gmail.com>
2018-11-08 13:17:21 +0800

    28. Implement strStr()

---
+++ 28.ImplementstrStr.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func strStr(haystack string, needle string) int {
+	if len(haystack) < len(needle) {
+		return -1
+	}
+	for i := 0; i <= len(haystack) - len(needle); i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(strStr("hello", "ll"))
+}
