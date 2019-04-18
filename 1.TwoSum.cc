05345373b838bbe5fc1bc4b69bd6641185fbe624
Zephyr <ixuhefeng@gmail.com>
2019-04-18 19:39:39 +0800

    1.two sum 12ms 10.1MB

--- 1.TwoSum.cc
+++ 1.TwoSum.cc
@@ -1,16 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = i + 1; j < nums.size(); ++j) {
-                if (nums[i] + nums[j] == target) {
-                    result.push_back(i);
-                    result.push_back(j);
-                    return result;
-                }
-            }
+        map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+			int diff = target - nums[i];
+			auto it = m.find(diff);
+			if (it != m.end()) {
+				result.push_back(it->second);
+				result.push_back(i);
+				return result;
+			}
+			m[nums[i]] = i;
         }
-        return result;
+		return result;
     }
 };
+
+int main() {
+	Solution a = Solution();
+	vector<int> v = {3, 3};
+	auto vec = a.twoSum(v, 6);
+	for (auto i : vec) {
+		cout << i << endl;
+	}
+	return 0;
+}
