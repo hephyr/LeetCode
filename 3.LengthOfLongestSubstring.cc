86af763cdb1092c0e491cca4cfef52ee95063f83
Zephyr <ixuhefeng@gmail.com>
2019-04-22 15:59:40 +0800

    Length of Longest Substring

--- 3.LengthOfLongestSubstring.cc
+++ 3.LengthOfLongestSubstring.cc
@@ -1,41 +1,30 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
 class Solution {
 public:
-    // first write
-    // int lengthOfLongestSubstring(string s) {
-    //     string max, temp;
-    //     for(auto c : s) {
-    //         for (auto t : temp) {
-    //             if (t == c) {
-    //                 if (temp.size() > max.size()) {
-    //                     max = temp;
-    //                 }
-    //                 auto a = temp.find(t);
-    //                 temp.erase(0, a+1);
-    //                 break;
-    //             }
-    //         }
-    //         temp.push_back(c);
-    //     }
-    //     if (temp.size() > max.size()) {
-    //         max = temp;
-    //     }
-    //     return max.size();
-    // }
-    // second write
     int lengthOfLongestSubstring(string s) {
+        int result = 0;
         map<char, int> m;
-        int maxSize = 0;
-        int lastRepeatPos = -1;
+        int start = 0;
         for (int i = 0; i < s.size(); ++i) {
-            if (m.find(s[i]) != m.end() && lastRepeatPos < m[s[i]]) {
-                lastRepeatPos = m[s[i]];
+            auto it = m.find(s[i]);
+            if (it != m.end() && it->second >= start) {
+                start = it->second + 1;
             }
-            int len = i - lastRepeatPos;
-            if (len > maxSize) {
-                maxSize = len;
-            }
+            int temp = i - start + 1;
+            result = result < temp ? temp : result;
             m[s[i]] = i;
+            if (s.size() - start + 1 <= result) break;
         }
-        return maxSize;
+        return result;
     }
 };
+
+int main() {
+    Solution s;
+    cout << s.lengthOfLongestSubstring("nfvbiywbasfbu") << endl;
+    cout << s.lengthOfLongestSubstring("abcabc") << endl;
+    return 0;
+}
