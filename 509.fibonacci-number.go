326e234493886ae76465dd347f32b8260d91292a
Zephyr <ixuhefeng@gmail.com>
2020-04-09 10:13:02 +0800

    [509] Fibonacci Number

---
+++ 509.fibonacci-number.go
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=509 lang=golang
+ *
+ * [509] Fibonacci Number
+ */
+
+// @lc code=start
+func fib(n int) int {
+    a, b := 0, 1
+    for n > 0 {
+        a, b = b, a + b
+        b %= 1000000007
+        n--
+    }
+    return a
+}
+// @lc code=end
+
